28c28677b3e1c00feac814696bab0b0d64a5d9bc
Vanina Diot <vanina.diot@gmail.com>
2019-03-30 00:24:46 +0100

    creationSuccess.php

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyrano
+ * Date: 2019-03-29
+ * Time: 22:57
+ */
+
+?>
+
+
---
+++ public/success.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyrano
+ * Date: 2019-03-30
+ * Time: 00:23
+ */
