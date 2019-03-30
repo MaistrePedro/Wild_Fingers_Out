6f3c40ac1d90893d4d5ab9e7e3fb2b7c27c70c47
maiwenn99 <maiwenngay99@gmail.com>
2019-03-30 01:55:48 +0100

    created bdd and add php

--- public/current/header.php
+++ public/current/header.php
@@ -4,8 +4,8 @@
     <div class="border-line-box"></div>
     <div class="page-box-title">
         <div class="container">
-            <h1>Share your .Idea</h1>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+            <h1><?php echo $pageTitle; ?></h1>
+            <p><?php echo $pageUnderTitle?></p>
         </div>
     </div>
 </section>
