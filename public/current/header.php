741b877e363f8ec5c4bae61a56f21329cf76c6fa
Vanina Diot <vanina.diot@gmail.com>
2019-03-30 03:23:32 +0100

    CSS1

--- public/current/header.php
+++ public/current/header.php
@@ -4,8 +4,11 @@
     <div class="border-line-box"></div>
     <div class="page-box-title">
         <div class="container">
-            <h1>Share your .Idea</h1>
+            <h1>Welcome to .Idea</h1>
+            <h2 class="typewriter">Let's make the world a better place with your .Idea !</h2>
+         <!--   <h1>Share your .Idea</h1>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+        -->
         </div>
     </div>
 </section>
